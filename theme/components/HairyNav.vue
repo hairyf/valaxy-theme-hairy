385d2d511ef790370a31fa164c45f051e803f35b
mrmao <wwu710632@gmail.com>
2022-07-14 17:52:43 +0800

    feat: hairy nav background transition

--- theme/components/HairyNav.vue
+++ theme/components/HairyNav.vue
@@ -15,5 +15,4 @@
 </template>
 
 <style>
-
 </style>
--- theme/components/HairyNavBackground.vue
+++ theme/components/HairyNavBackground.vue
@@ -1,3 +1,9 @@
 <template>
-  <div class="absolute inset-0 bg-white bg-opacity-50 dark:bg-black dark:bg-opacity-50 blur-5" />
+  <div class="HairyNavBackground absolute inset-0 bg-white bg-opacity-50 dark:bg-black dark:bg-opacity-50 blur-5" />
 </template>
+
+<style>
+.HairyNavBackground {
+  transition: background 0.35s;
+}
+</style>
