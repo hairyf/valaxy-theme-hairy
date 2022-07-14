d44a684841319451d5323d215e25d40921a68349
mrmao <wwu710632@gmail.com>
2022-07-14 17:50:17 +0800

    chore: update hairy component

--- theme/components/HairyNav.vue
+++ theme/components/HairyNav.vue
@@ -1,14 +1,3 @@
-<script setup lang="ts">
-import { useI18n } from 'vue-i18n'
-import { computed } from 'vue'
-import { isDark, toggleDark } from 'valaxy'
-
-const { t } = useI18n()
-const themeTitle = computed(() => {
-  return isDark.value ? t('button.toggle_light') : t('button.toggle_dark')
-})
-</script>
-
 <template>
   <div class="HairyNav fixed w-full h-3.125rem top-0">
     <div class="mx-auto container flex relative z-1">
@@ -17,13 +6,11 @@ const themeTitle = computed(() => {
         <HairyMenu />
       </div>
       <div class="flex-center">
-        <button class="yun-icon-btn" :title="themeTitle" :style="{ color: isDark ? '' : '#f1cb64' }" @click="toggleDark()">
-          <div i="ri-sun-line dark:ri-moon-line" />
-        </button>
+        <HairyNavToggleDark />
         <HairyNavSearch />
       </div>
     </div>
-    <div class="absolute inset-0 bg-white bg-opacity-50 dark:bg-black dark:bg-opacity-50 blur-5"></div>
+    <HairyNavBackground />
   </div>
 </template>
 
---
+++ theme/components/HairyNavBackground.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="absolute inset-0 bg-white bg-opacity-50 dark:bg-black dark:bg-opacity-50 blur-5" />
+</template>
