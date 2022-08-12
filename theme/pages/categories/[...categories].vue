7286d7adb200ebab5950f383de0475c7a16dd418
mrmao <wwu710632@gmail.com>
2022-08-12 18:06:01 +0800

    fix: post list and categories and tag

--- theme/pages/categories/[...categories].vue
+++ theme/pages/categories/[...categories].vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed, watchEffect } from 'vue'
+import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRouter } from 'vue-router'
 import { useThemeConfig } from 'valaxy'
