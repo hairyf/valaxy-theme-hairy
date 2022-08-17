faff4b653b51add39ce37e5431de3eac7390269c
mrmao <wwu710632@gmail.com>
2022-08-17 15:47:25 +0800

    chore: change desc hover color

--- theme/components/HairyLinks.vue
+++ theme/components/HairyLinks.vue
@@ -14,7 +14,10 @@ defineProps<{
 <template>
   <div class="min-h-30vh">
     <div class="links">
-      <div v-for="(item, index) in links" :key="index" class="link-block flex items-center py-0.5rem px-1rem rounded-lg" :style="{ '--block-color': item.color }">
+      <div
+        v-for="(item, index) in links" :key="index" class="link-block flex items-center py-0.5rem px-1rem rounded-lg"
+        :style="{ '--block-color': item.color }"
+      >
         <a :href="item.url" class="w-4rem h-4rem">
           <HairyImage class="w-full h-full rounded-xl" :src="item.image" />
         </a>
@@ -22,7 +25,7 @@ defineProps<{
           <a :href="item.url" class="font-bold text-lg title">
             {{ item.name }}
           </a>
-          <div class="max-w-180px text-sm my-0.5rem truncate">
+          <div class="max-w-180px text-sm my-0.5rem truncate desc">
             {{ item.desc }}
           </div>
         </div>
@@ -34,23 +37,28 @@ defineProps<{
 <style lang="scss" scoped>
 .link-block {
   border: 0.0625rem solid #f7f7f7;
-  box-shadow: 0 0.625rem 1.875rem -0.9375rem rgba(0,0,0,0.1);
+  box-shadow: 0 0.625rem 1.875rem -0.9375rem rgba(0, 0, 0, 0.1);
   --bg-color: var(--block-color, #666);
   @apply transition-all;
+
   .title {
     color: var(--block-color);
   }
 }
+
 .dark .link-block {
   background: rgba($color: #989898, $alpha: 0.1);
 }
+
 .links .link-block:hover {
-    background-color: var(--bg-color);
-    box-shadow: 0 0.125rem 1.25rem var(--bg-color);
-    border-color: var(--bg-color);
-    .title {
-      color: #fff;
-    }
+  background-color: var(--bg-color);
+  box-shadow: 0 0.125rem 1.25rem var(--bg-color);
+  border-color: var(--bg-color);
+
+  .title,
+  .desc {
+    color: #fff;
+  }
 }
 
 .links {
