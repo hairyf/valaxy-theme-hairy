3f5a41f4b9f41fa750c3e34390055e1befe4c3a5
hairyf <hairyf@users.noreply.github.com>
2025-12-22 09:59:54 +0000

    deploy: 98181df77407385339952905e2d75dca4d725379

---
+++ assets/index-CBLoIDtx.js
@@ -0,0 +1,1 @@
+import{b as n}from"./route-block-B_A1xBdJ.js";import{_ as r}from"./app-B_z-Xn6H.js";const o={};function t(c,e){return null}typeof n=="function"&&n(o);const s=r(o,[["render",t]]);export{s as default};
